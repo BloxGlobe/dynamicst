ff7daac11783819b129444fcc46bd8cf8aa498b7
Abel <malebochester@gmail.com>
2026-01-26 18:23:34 +0200

    update files

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './components/styles/foundation.css'
 import './components/styles/footer.css'
 import './components/styles/sidebar.css'
+import './components/landing/landing.tsx'
 
 function App() {
   return (
